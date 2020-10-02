e5cea09c80d99c64f6baa3c5515876636b036898
Chris Lee <chrislee187@gmail.com>
2020-10-02 11:05:54 +0100

    AoC 2015-10 part 1 complete

---
+++ 2015/AoC2015-10/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AoC2015_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lookNSay = new LookNSay();
+
+            // Console.Write("Source Input: ");
+            // var input = Console.ReadLine();
+            // Console.Write("iterations: ");
+            // var iterations = int.Parse(Console.ReadLine());
+
+            var input = "1321131112";
+            var iterations = 40;
+            for (int i = 0; i < iterations; i++)
+            {
+                var result = lookNSay.Do(input);
+
+                Console.WriteLine($"#{i:00} ({result.Length})");
+                Debug.Assert(result.Length == 492982, "Part 1 failed");
+
+                input = result;
+
+            }
+
+
+        }
+    }
+
+    internal class LookNSay
+    {
+        public string Do(string input)
+        {
+            var currentDigit = '\0';
+            var currentCount = 0;
+            var result = "";
+            foreach (var c in input)
+            {
+                if (currentDigit == c)
+                {
+                    currentCount++;
+                }
+                if (currentDigit != c)
+                {
+                    if (currentDigit != '\0')
+                    {
+                        result += $"{currentCount}{currentDigit}";
+                    }
+                    currentDigit = c;
+                    currentCount = 1;
+                }
+            }
+
+            if (currentCount > 0)
+            {
+                result += $"{currentCount}{currentDigit}";
+            }
+            return result;
+        }
+    }
+}
